ba781b6084a439d9f0de3c70d877a161da5e2284
bredele <olivier.wietrich@gmail.com>
2013-12-21 20:57:31 -0700

    start merge tests

---
+++ test/merge.js
@@ -0,0 +1,18 @@
+var carry = require('../'),
+    assert = require('assert');
+
+describe("merge", function() {
+
+	it("shoud merge an object with carry apps", function() {
+		carry.merge({
+			path: function(){}
+		});
+		var app = carry();
+
+	});
+
+	it("should merge multiple objects", function() {
+		
+	});
+	
+});
