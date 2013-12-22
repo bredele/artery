e2be11b1de4652f3d30750d52c5ddbada33e6ec5
bredele <olivier.wietrich@gmail.com>
2013-12-21 21:23:49 -0700

    merge multiple objects globally in carry, fixes #1

--- test/merge.js
+++ test/merge.js
@@ -1,18 +1,23 @@
-var carry = require('../'),
-    assert = require('assert');
+var assert = require('assert'),
+    carry = require('carry').merge({
+			path: function(){}
+		}, {
+			get: function(){}
+		});
 
 describe("merge", function() {
+	var app = null;
+	beforeEach(function(){
+		app = carry();
+	});
 
 	it("shoud merge an object with carry apps", function() {
-		carry.merge({
-			path: function(){}
-		});
-		var app = carry();
-
+		assert(typeof app.path === 'function');
 	});
 
 	it("should merge multiple objects", function() {
-		
+		assert(typeof app.path === 'function');
+		assert(typeof app.get === 'function');
 	});
 	
 });
