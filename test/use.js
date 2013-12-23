368c4a68b6d188e8f5b8b848e152f84ce91663be
bredele <olivier.wietrich@gmail.com>
2013-12-22 20:34:43 -0700

    initialize test for carry middleware

---
+++ test/use.js
@@ -0,0 +1,10 @@
+var assert = require('assert'),
+    carry = require('carry');
+
+describe("Carry middleware", function() {
+	var app = null;
+	beforeEach(function(){
+		app = carry();
+	});
+	
+});
