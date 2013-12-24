e1ec6a07f5d1bf1e639e2769bcfb87dd21b189af
bredele <olivier.wietrich@gmail.com>
2013-12-23 22:30:25 -0700

    add communication events

--- test/emitter.js
+++ test/emitter.js
@@ -3,18 +3,43 @@ var assert = require('assert'),
 
 describe("Communication bus", function() {
 	var app = null;
+
 	beforeEach(function(){
 		app = carry();
 	});
 	
 	it("should prefix events with app's name", function() {
-		var call;
+		var r = false;
 		app.name = 'bredele';
 		app.on('bredele/test', function(val){
-			call = val;
+			r = val;
 		});
 		app.emit('test', true);
-		assert.equal(call, true);
+		assert.equal(r, true);
+	});
+
+	it("should remove event listener", function() {
+		var idx = 0,
+		fn = function(){
+			idx++;
+		};
+		app.name = 'bredele';
+		app.on('bredele/test', fn);
+		app.emit('test');
+		app.off('bredele/test', fn);
+		app.emit('test');
+		assert.equal(idx, 1);
+	});
+
+	it("should emit event once", function() {
+		var idx = 0;
+		app.name = 'bredele';
+		app.once('bredele/test', function(){
+			idx++;
+		});
+		app.emit('test');
+		app.emit('test');
+		assert.equal(idx, 1);
 	});
 	
 });
