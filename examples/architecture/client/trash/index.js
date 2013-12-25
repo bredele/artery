31110e107b886d39e294ddb4ce704c3324ada357
bredele <olivier.wietrich@gmail.com>
2013-12-24 21:14:25 -0700

    update example

--- examples/architecture/client/trash/index.js
+++ examples/architecture/client/trash/index.js
@@ -1,6 +1,9 @@
+var list = document.querySelector('.list');
 
 var app = module.exports = require('carry')();
 
 	app.on('mails/delete', function() {
-		console.log('trash');
+		var li = document.createElement('li');
+		li.innerText = 'mail deleted';
+		list.appendChild(li);
 	});
