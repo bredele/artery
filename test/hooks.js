60249c8aaf542f33a226c5ef5dfd1f5dd5aa5cbc
bredele <olivier.wietrich@gmail.com>
2013-12-26 21:36:47 -0700

    add test init hook

--- test/hooks.js
+++ test/hooks.js
@@ -3,8 +3,22 @@ var assert = require('assert'),
 
 describe("Hooks", function() {
 
+	var app = null,
+	    hub = null;
+
+	beforeEach(function(){
+		app = artery();
+		hub = artery();
+	});
+
 	describe("init", function() {
-		
+		var called = false;
+		app.sandbox.on('init', function(){
+			called = true;
+		});
+		assert.equal(called, false);
+		hub.use('app', app);
+		qssert.equal(called, true);
 	});
 	describe("stop", function() {
 		
