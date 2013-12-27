9bc9e51ef07e6491a42602f406b189b4477cf56a
bredele <olivier.wietrich@gmail.com>
2013-12-26 19:36:30 -0700

    initialize hook tests

---
+++ test/hooks.js
@@ -0,0 +1,15 @@
+var assert = require('assert'),
+    artery = require('artery');
+
+describe("Hooks", function() {
+
+	describe("init", function() {
+		
+	});
+	describe("stop", function() {
+		
+	});
+	describe("destroy", function() {
+		
+	});
+});
